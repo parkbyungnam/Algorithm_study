93874aae2f57ddc5a625495faa2300a7639d8659
Park_ByungNam <tom3k@naver.com>
2020-10-30 23:07:11 +0900

    maze

---
+++ baejoon/단계별문제/DFS_BFS/1260.py
@@ -0,0 +1,4 @@
+n, m = map(int,input().split())
+maze = []
+for _ in range(n):
+    maze = list(int(input()))
---
+++ nadongbin/DFS_BFS/미로탈출.py
@@ -0,0 +1,47 @@
+'''
+5 6
+101010
+111111
+000001
+111111
+111111
+'''
+
+
+from collections import deque
+
+'''
+n, m = map(int,input().split())
+maze = []
+for _ in range(n):
+    maze.append(list(map(int,input())))
+'''
+
+n, m = 5,6
+maze = [[1,0,1,0,1,0],[1,1,1,1,1,1],[0,0,0,0,0,1],[1,1,1,1,1,1],[1,1,1,1,1,1]]
+
+def bfs(maze, n, m, a, b):
+    queue = deque()
+    queue.append((a,b))
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dx[i]
+            if nx<0 or nx>=n or ny<0 or ny>=m:
+                continue
+            if maze[nx][ny] == 0:
+                continue
+            if maze[nx][ny] == 1:
+                maze[nx][ny] = maze[x][y] + 1
+                queue.append((nx,ny))
+    return maze[n-1][m-1]
+
+print(bfs(maze,n,m,0,0))
+
+
+
+
+
