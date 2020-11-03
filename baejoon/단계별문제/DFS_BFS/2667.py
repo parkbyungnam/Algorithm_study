68b3f8e9d3e76fd4f5c6e5805d92aee0f68369b7
Park_ByungNam <tom3k@naver.com>
2020-11-04 00:00:21 +0900

    Update 2667.py

--- baejoon/단계별문제/DFS_BFS/2667.py
+++ baejoon/단계별문제/DFS_BFS/2667.py
@@ -52,3 +52,40 @@ for i in range(total+1,1,-1):
 cnt_arr.sort()
 for cnt in cnt_arr:
     print(cnt)
+
+#타인 풀이
+
+import sys
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+n = int(sys.stdin.readline())
+a = [list(sys.stdin.readline()) for _ in range(n)]
+cnt = 0
+apt = []
+
+
+def dfs(x, y):
+    global cnt
+    a[x][y] = '0'
+    cnt += 1
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx < 0 or nx >= n or ny < 0 or ny >= n:
+            continue
+        if a[nx][ny] == '1':
+            dfs(nx, ny)
+
+
+for i in range(n):
+    for j in range(n):
+        if a[i][j] == '1':
+            cnt = 0
+            dfs(i, j)
+            apt.append(cnt)
+
+print(len(apt))
+apt.sort()
+for i in apt:
+    print(i)
