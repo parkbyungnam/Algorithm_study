53497abd62f5275598dba750344e6cc6948284c4
Park_ByungNam <tom3k@naver.com>
2020-11-05 13:23:21 +0900

    Update 7576.py

--- baejoon/단계별문제/DFS_BFS/7576.py
+++ baejoon/단계별문제/DFS_BFS/7576.py
@@ -2,21 +2,16 @@ from collections import deque
 
 M,N = map(int,input().split())
 graph = []
+
+for _ in range(N):
+    graph.append(list(map(int,input().split())))
+
 where_is_1=[]
 
-
-for i in range(N):
-    graph.append(list(map(int,input().split())))
-    for j in range(M):
-        if graph[i][j] == 1:
-            where_is_1.append([i,j])
-
-'''
 for i,grap in enumerate(graph):
     for j,gra in enumerate(grap):
         if graph[i][j] == 1:
             where_is_1.append([i,j])
-'''
 
 def bfs():
     global graph
@@ -49,3 +44,54 @@ def bfs():
     return max-1
 
 print(bfs())
+
+
+#타인 소스
+
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+
+def solve():
+    m, n = map(int, input().split())
+    tomato = []
+    haveto = 0
+    tmt = deque()
+    for i in range(n):
+        tomato.append(input().split())
+        for j in range(m):
+            if tomato[i][j] == '0':
+                haveto += 1
+            elif tomato[i][j] == '1':
+                tmt.append((i, j))
+    res = 0
+    while tmt and haveto:
+        l = len(tmt)
+        for _ in range(l):
+            x, y = tmt.popleft()
+            if x > 0 and tomato[x-1][y] == '0':
+                tomato[x-1][y] = 1
+                tmt.append((x-1, y))
+                haveto -= 1
+            if y > 0 and tomato[x][y-1] == '0':
+                tomato[x][y-1] = 1
+                tmt.append((x, y-1))
+                haveto -= 1
+            if x < n-1 and tomato[x+1][y] == '0':
+                tomato[x+1][y] = 1
+                tmt.append((x+1, y))
+                haveto -= 1
+            if y < m-1 and tomato[x][y+1] == '0':
+                tomato[x][y+1] = 1
+                tmt.append((x, y+1))
+                haveto -= 1
+        res += 1
+    if haveto:
+        print(-1)
+    else:
+        print(res)
+
+
+if __name__ == '__main__':
+    solve()
