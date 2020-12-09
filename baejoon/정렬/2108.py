478317b4cf14f4c6ae6a8785895ed14f98499760
Park_ByungNam <tom3k@naver.com>
2020-12-09 18:31:55 +0900

    Update 2108.py

--- baejoon/정렬/2108.py
+++ baejoon/정렬/2108.py
@@ -1,25 +1,35 @@
+from collections import Counter
+
 testCase = int(input())
 mylist = []
 for _ in range(testCase):
     mylist.append(int(input()))
 
 mylist.sort()
-print(sum(mylist)//testCase) #산술평균
+print(round(sum(mylist)/testCase)) #산술평균
 print(mylist[testCase//2]) #중앙값
 
-count = 0
-for my in mylist:
-    myCount = mylist.count(my)
-    if myCount>count:
-        count = myCount
-        countlist = []
-        countlist.append(my)
-    elif myCount == count:
-        countlist.append(my)
-
-if countlist[-1]==countlist[0]:
-    print(countlist[0])
+#최빈값
+mycount = Counter(mylist).most_common(2)
+if len(mycount)==1:
+    print(mycount[0][0])
 else:
-    print(countlist[1]) #최빈값
+    if mycount[0][1]==mycount[1][1]:
+        print(mycount[1][0])
+    else:
+        print(mycount[0][0])
 
 print(mylist[-1]-mylist[0]) #범위
+
+
+#my Count
+def countLetters(word):
+    counter = {}
+    for letter in word:
+        if letter not in counter:
+            counter[letter] = 0
+        counter[letter] += 1
+    return counter
+
+countLetters('hello world')
+# {'h': 1, 'e': 1, 'l': 3, 'o': 2, ' ': 1, 'w': 1, 'r': 1, 'd': 1}
