8e940f6157b8bfb9a1929504e3e6b7261677630f
Park_ByungNam <tom3k@naver.com>
2020-12-09 23:35:15 +0900

    Update 11651.py

--- baejoon/정렬/11651.py
+++ baejoon/정렬/11651.py
@@ -4,7 +4,11 @@ testCase = int(input())
 mylist = []
 for _ in range(testCase):
     mylist.append(list(map(int,input().split())))
-sortedlist = sorted(mylist,key=itemgetter(1,0))
+sortedlist = sorted(mylist,key=itemgetter(1,0)) #itemgetter , attrgetter
 for i in sortedlist:
     x,y=i
     print(x,y)
+
+
+
+#참고자료 https://docs.python.org/ko/3/howto/sorting.html
