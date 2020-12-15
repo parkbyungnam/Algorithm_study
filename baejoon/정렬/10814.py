a3abc08f7186aee742ef439496ded5f6edaa80b7
Park_ByungNam <tom3k@naver.com>
2020-12-15 23:39:08 +0900

    Update 10814.py

--- baejoon/정렬/10814.py
+++ baejoon/정렬/10814.py
@@ -8,3 +8,18 @@ mylist.sort(key=itemgetter(0,2))
 for my in mylist:
     age,name,i = my
     print(age,name)
+
+# rstrip 확인하자
+from operator import itemgetter
+import sys
+
+input = sys.stdin.readline
+testCase = int(input())
+mylist = []
+for i in range(testCase):
+    age,name = input().rstrip().split()
+    mylist.append([int(age),name,i])
+mylist.sort(key=itemgetter(0,2))
+for my in mylist:
+    age,name,i = my
+    print(age,name)
