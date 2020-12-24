0c98a6f1976543d4c482e7326bdc794e45298065
Park_ByungNam <tom3k@naver.com>
2020-12-24 22:21:42 +0900

    Update 15651.py

--- baejoon/백트래킹/15651.py
+++ baejoon/백트래킹/15651.py
@@ -1,14 +1,41 @@
-from itertools import permutations
+#참고하면 좋은 자료
+#https://ghwlchlaks.github.io/permutation-combination-python
+
 
 N,M = map(int,input().split())
+result = [0 for _ in range(M)]
 
-mylist = [i for i in range(N,N+1)]
-for i in range(N**M):
-    for 
+def solution(index,N,M):
+    if index==M:
+        for re in result:
+            print(re,end=" ")
+        print()
+        return
+    else:
+        for i in range(1,N+1):
+            result[index]=i
+            solution(index+1,N,M)
+
+solution(0,N,M)
 
 
+#타인 풀이
 
+import sys
+from itertools import product
 
+def testCase():
+    input_val = list(map(int, sys.stdin.readline().split()))
+    global N, M
+    N, M = input_val
+
+def solve(remain):
+    res = list(map(' '.join, product(remain, repeat=M)))
+    print('\n'.join(res))
+
+if __name__ == "__main__":
+    testCase()
+    solve(map(str, list(range(1, N+1))))
 
 
 #타인 풀이
