1f2c43823fe530ef3930c4e8b13cc94c33293a34
Park_ByungNam <tom3k@naver.com>
2020-12-25 14:43:46 +0900

    Update 14888.py

--- baejoon/백트래킹/14888.py
+++ baejoon/백트래킹/14888.py
@@ -43,3 +43,40 @@ for operator in myOperator:
 
 print(myMax)
 print(myMin)
+
+
+
+#타인풀이
+
+def dfs(cal, idx, plus, minus, multiple, division):
+    global max_num, min_num
+    if idx == N:
+        if max_num < cal:
+            max_num = cal
+        if min_num > cal:
+            min_num = cal
+        return
+    else:
+        if plus != 0:
+            dfs(cal + arr[idx], idx + 1, plus - 1, minus, multiple, division)
+        if minus != 0:
+            dfs(cal - arr[idx], idx + 1, plus, minus - 1, multiple, division)
+        if multiple != 0:
+            dfs(cal * arr[idx], idx + 1, plus, minus, multiple - 1, division)
+        if division != 0:
+            # dfs(cal // arr[idx], idx + 1, plus, minus, multiple, division - 1)
+            dfs(int(cal / arr[idx]), idx + 1, plus, minus, multiple, division - 1)
+
+
+N = int(input())
+arr = list(map(int, input().split()))
+plus, minus, multiple, division = list(map(int, input().split()))
+result = []
+max_num = -1000000000
+min_num = 1000000000
+
+dfs(arr[0], 1, plus, minus, multiple, division)
+
+print(max_num)
+print(min_num)
+
