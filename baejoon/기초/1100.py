6ba0eaf268df1e7cd325b8c7170601e753a6c05a
Park_ByungNam <tom3k@naver.com>
2020-12-29 23:09:09 +0900

    Update 1100.py

--- baejoon/기초/1100.py
+++ baejoon/기초/1100.py
@@ -3,10 +3,10 @@ mylist = []
 for _ in range(8):
     mylist.append(list(input()))
 
-for i,my in enumerate(mylist):
-    for j,m in enumerate(my):
-        if i+j%2==1:
-            m='.'
+for i in range(8):
+    for j in range(8):
+        if (i+j)%2==1:
+            mylist[i][j]='.'
 
 result = 0
 for my in mylist:
