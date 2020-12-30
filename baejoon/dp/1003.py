0f9b6713530e60c311d6e7ad0f3dcf314522050d
Park_ByungNam <tom3k@naver.com>
2020-12-30 23:47:24 +0900

    Create 1003.py

---
+++ baejoon/dp/1003.py
@@ -0,0 +1,29 @@
+def fibo0(n):
+    if n==1:
+        my0 = 0
+    elif n<=3:
+        my0 = 1
+    else:
+        past = 1
+        my0 = 1
+        for _ in range(n-3):
+            temp = my0
+            my0 += past
+            past = temp
+    return my0
+
+def fibo1(m):
+    if m==0:
+        my1 = 0
+    else:
+        my1 = 1
+        past = 1
+        for _ in range(m-2):
+            temp = past
+            my1 += past
+            past = temp
+    return my1
+
+def solution(n):
+    print(fibo0(n),fibo1(n))
+
