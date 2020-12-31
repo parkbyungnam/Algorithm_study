267cc7e8c8845adc8b00007b88587bc5370f3741
Park_ByungNam <tom3k@naver.com>
2020-12-31 23:36:41 +0900

    Update 1904.py

--- baejoon/dp/1904.py
+++ baejoon/dp/1904.py
@@ -32,7 +32,6 @@ def solution(n):
             past = temp
         print(current%15746)
 
-
 solution(int(input()))
 
 #다른 풀이
@@ -41,7 +40,7 @@ dp = [0] * 1000001
 dp[1] = 1
 dp[2] = 2
 
-for i in range(3,1000001):
-    dp[i] = dp[i-1] + dp[i-2]
+for i in range(3,n+1):
+    dp[i] = (dp[i-1] + dp[i-2])%15746
 
-print(n)
+print(dp[n])
