59918eacc087a3429cbe16886650b47d469bb5a9
Park_ByungNam <tom3k@naver.com>
2021-01-10 21:10:57 +0900

    Update 4949.py

--- baejoon/스택/4949.py
+++ baejoon/스택/4949.py
@@ -1,32 +1,44 @@
 def checkBalance(myString):
-    small = 0
-    large = 0
+    myList = []
     flag = True
     for st in myString:
         if st == '(':
-            small += 1
-            flag2 = 1
+            myList.append('(')
         elif st == ')':
-            small -= 1
+            if not myList:
+                flag = False
+                break
+            else:
+                temp = myList.pop()
+                if temp != '(':
+                    flag = False
+                    break
+
         elif st == '[':
-            large += 1
-            flag2 = 2
+            myList.append('[')
         elif st == ']':
-            large -= 1
-        if small < 0 or large < 0:
-            flag = False
-    if small == 0 and large == 0 and flag:
+            if not myList:
+                flag = False
+                break
+            else:
+                temp = myList.pop()
+                if temp != '[':
+                    flag = False
+                    break
+    if not myList and flag:
         return True
     else:
         return False
 
-flag = True
-while flag:
-    myString = input()
-    if myString == '.':
-        flag=False
-    else:
-        if checkBalance(myString):
-            print('yes')
+if __name__=='__main__':
+    flag = True
+    while flag:
+        myString = input()
+        if myString == '.':
+            flag = False
+            break
         else:
-            print('no')
+            if checkBalance(myString):
+                print('yes')
+            else:
+                print('no')
