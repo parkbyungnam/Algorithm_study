f5f3b4b729c9bb665b8eaedcdeab3e92be46d31d
Park_ByungNam <tom3k@naver.com>
2021-01-13 23:48:54 +0900

    Update 11866.py

--- baejoon/큐/11866.py
+++ baejoon/큐/11866.py
@@ -12,3 +12,15 @@ while josephusProblem:
     if josephusProblem:
         print(', ', end='')
 print('>')
+
+
+#타인 풀이
+N, K = map(int, input().split())
+l = list(range(1, N+1))
+p = list()
+i = 0
+while l:
+    i = (i+K-1) % len(l)
+    p.append(str(l.pop(i)))
+
+print('<'+', '.join(p)+'>')
