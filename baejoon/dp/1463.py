fb896331b915ae7d1c6950d83e1d06a1b5d96f20
Park_ByungNam <tom3k@naver.com>
2021-02-26 16:04:09 +0900

    1463

--- baejoon/dp/1463.py
+++ baejoon/dp/1463.py
@@ -1,28 +1,17 @@
-#bfs? 나중에 다시 풀어보자.
+#정답
+#10과 같은 수를 처리하는 것이 관건
+#dp(n) = min(dp[n//3]+1 , dp[n//2]+1 , dp[n-1]+1)
+
 
 n = int(input())
 dp = [0 for _ in range(n+1)]
-for i in range(2, n+1):
-    dp[i] = dp[i-1] + 1  
-    if i%2 == 0 and dp[i] > dp[i//2] + 1 :
-        dp[i] = dp[i//2] + 1
-    elif i%3 == 0 and dp[i] > dp[i//3] + 1 :
-        dp[i] = dp[i//3] + 1
-        
+for i in range(2,n+1):
+    dp[i]=dp[i-1]+1
+    if i%2==0 and dp[i//2]+1 < dp[i]:
+        dp[i]=dp[i//2]+1
+    if i%3==0 and dp[i//3]+1 < dp[i]:
+        dp[i]=dp[i//3]+1
 print(dp[n])
-
-
-
-n = int(input())
-count = 0
-while n!=1:
-    if n%3 == 0:
-        n//=3
-        count += 1
-    else:
-        n-=1
-        count += 1
-print(count)
 
 
 '''
